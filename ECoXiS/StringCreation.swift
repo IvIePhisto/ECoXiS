adbf4e531e242d64f3f72d6522140d91a71bf74a
Michael Pohl <pohl-michael@gmx.net>
2014-06-18 17:44:40 +0200

    Started with XML string creation * added StringCreation.swift, document creation still missing * started with test for string creation

---
+++ ECoXiS/StringCreation.swift
@@ -0,0 +1,46 @@
+operator prefix &< {}
+
+@prefix func &< (content: String) -> String {
+    return XMLUtilities.escape(content)
+}
+
+operator prefix !< {}
+
+@prefix func !< (content: String) -> String {
+    let c = XMLUtilities.enforceCommentContent(content)
+    return XMLComment.createString(c)
+}
+
+func pi(target: String, _ value: String? = nil) -> String {
+    let maybeTarget = XMLUtilities.enforceProcessingInstructionTarget(target)
+
+    if let t = maybeTarget {
+        let v = XMLUtilities.enforceProcessingInstructionValue(value)
+        return XMLProcessingInstruction.createString(t, value: v)
+    }
+
+    return ""
+}
+
+func el(name: String, _ attributes: Dictionary<String, String> = [:],
+        children: () -> String) -> String {
+    let maybeName = XMLUtilities.enforceName(name)
+
+    if let n = maybeName {
+        let c = children()
+        var preparedAttributes = Dictionary<String, String>()
+
+        for (name, value) in attributes {
+            if let n = XMLUtilities.enforceName(name) {
+                let v = XMLUtilities.escape(value, .EscapeQuot)
+                preparedAttributes[n] = v
+            }
+        }
+
+        let a = XMLAttributes.createString(preparedAttributes.generate())
+        return XMLElement.createString(n, attributesString: a,
+            childrenString: c)
+    }
+
+    return ""
+}
--- ECoXiSTests/BasicTest.swift
+++ ECoXiSTests/BasicTest.swift
@@ -15,8 +15,14 @@ class BasicTest: XCTestCase {
         let text = "<Hello World!/>"
         var fooValue = "<foz'\">"
         var escapedFooValue = "&lt;foz'&quot;&gt;"
-        var element = </"<test/>" + ["!foo": fooValue, "<bar/>": "boz"]
+        var attributes = ["!foo": fooValue, "<bar/>": "boz"]
+        var element = </"<test/>" + attributes
             + [<&text, PI("foo", "bar"), <!"--foo--bar--"]
+
+        println(el("!Foo Bar", attributes) {
+            !<"--foo--bar--" + &<fooValue + pi("foo", "bar")
+        })
+
         XCTAssert(element.name == "test")
         XCTAssert(element[0]! as XMLText == text)
         XCTAssert(text == element[0]! as XMLText)
@@ -32,10 +38,10 @@ class BasicTest: XCTestCase {
         XCTAssert(!element.attributes.contains("bar"))
         var document = XML(element, doctype: Doctype())
         XCTAssert(document.toString() == "<?xml version=\"1.0\"?><!DOCTYPE FooBar><FooBar foo=\"\(escapedFooValue)\">&lt;Hello World!/&gt;<?foo bar?><!--foo-bar--></FooBar>")
-        let pi = element[1] as XMLProcessingInstruction
-        pi.value = "bar?> ? >"
-        XCTAssert(pi.toString() == "<?foo bar? ? >?>")
-        pi.target = nil
+        let processingInstruction = element[1] as XMLProcessingInstruction
+        processingInstruction.value = "bar?> ? >"
+        XCTAssert(processingInstruction.toString() == "<?foo bar? ? >?>")
+        processingInstruction.target = nil
         element[0] = nil
         element[0] = nil
         element[0] = nil
